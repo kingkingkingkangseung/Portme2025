ce2defa9e373ce42dc98ee807d884ac2506724ae
동하 <dongha.kwon1116@gmail.com>
2025-08-01 01:00:54 +0900

    fix: install system deps, adjust postdeploy hook, fix Procfile and psycopg version

--- .platform/hooks/postdeploy/01_migrate.sh
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-# RDS, EB 환경변수 로딩
-source /opt/elasticbeanstalk/support/envvars
-
-# 앱 코드가 체크아웃된 디렉터리로 이동
+# 코드 위치 이동
 cd /var/app/current
 
 # 가상환경 활성화
 source /var/app/venv/*/bin/activate
 
-# 마이그레이션 실행
+# 마이그레이션
 python manage.py migrate --noinput
-
-# 필요하다면 스태틱 수집
+# 정적파일 수집
 python manage.py collectstatic --noinput
