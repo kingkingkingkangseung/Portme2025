c11ad223bd19972f29b5bdf946e910ae6a13301f
동하 <dongha.kwon1116@gmail.com>
2025-08-01 14:34:24 +0900

    chore: add whitenoise, fix migrate hook, ensure envvars

--- .platform/hooks/postdeploy/01_migrate.sh
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-# elasticbeanstalk가 설치한 가상환경에 환경변수 로드
-source /opt/elasticbeanstalk/.env
-# (또는 /opt/elasticbeanstalk/support/envvars 가 존재하면 그걸로)
+# EB 인스턴스 환경 변수를 로드
+source /opt/elasticbeanstalk/support/envvars
 
-# 마이그레이션
-/var/app/venv/*/bin/python manage.py migrate --noinput
+# Django 마이그레이션 수행
+python3 manage.py migrate --noinput
