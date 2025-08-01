454a89a155b1d99e26e97ae6f7cb148b60e3b205
동하 <dongha.kwon1116@gmail.com>
2025-08-01 20:24:15 +0900

    feat: add login and community features

--- apps/user/models.py
+++ apps/user/models.py
@@ -2,4 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    pass  # 지금은 필드 없어도 OK, 나중에 필요하면 추가
+    # 커뮤니티 필드
+    bio = models.TextField(blank=True)
+    # 로그인/소셜필드
+    social_id = models.CharField(max_length=255, blank=True)
