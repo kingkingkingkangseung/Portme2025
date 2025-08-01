f236ad26135160b6ebb0f9306f4c2b062f933a82
동하 <dongha.kwon1116@gmail.com>
2025-08-01 15:42:32 +0900

    fix: postdeploy hook activate venv & migrate

--- .platform/hooks/postdeploy/01_migrate.sh
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-# EB_VIRTUAL_ENV는 EB가 자동으로 설정해 주는 환경변수입니다
-source "${EB_VIRTUAL_ENV}/bin/activate"
-cd "${EB_APP_CURRENT}"       # 앱 루트 디렉터리로 이동
+# 1) 애플리케이션이 설치된 디렉터리로 이동
+cd /var/app/current
+
+# 2) EB가 만든 가상환경 활성화
+source /var/app/venv/*/bin/activate
+
+# 3) 마이그레이션 실행
 python manage.py migrate --noinput
