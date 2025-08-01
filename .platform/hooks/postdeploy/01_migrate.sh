c8daf9b7a4da3f299f88c77f57992cea3004ceee
동하 <dongha.kwon1116@gmail.com>
2025-08-01 15:40:06 +0900

    chore: make postdeploy hook executable

--- .platform/hooks/postdeploy/01_migrate.sh
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+set -e
+
+# EB_VIRTUAL_ENV는 EB가 자동으로 설정해 주는 환경변수입니다
 source "${EB_VIRTUAL_ENV}/bin/activate"
-cd "${EB_APP_CURRENT}"
+cd "${EB_APP_CURRENT}"       # 앱 루트 디렉터리로 이동
 python manage.py migrate --noinput
