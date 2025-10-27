e16ac382976c69c21d9ea9753ff8625889b9ef69
kwondongha1116 <dongha.kwon1116@gmail.com>
2025-10-27 20:15:22 +0900

    프로젝트 수정 및 박강승 코드 합침

--- config/urls.py
+++ config/urls.py
@@ -20,54 +20,17 @@ from apps.activity.views import AwardViewSet, CertificationViewSet
 def health(request):
     return HttpResponse("OK")
 
-<<<<<<< HEAD
-def google_callback_echo(request):
-    code = request.GET.get("code")
-    if not code:
-        return JsonResponse({"detail": "code not found"}, status=400)
-    state = request.GET.get("state")
-    return JsonResponse({"code": code, "state": state}, status=200)
-
-def oauth_callback_echo(request):
-    return HttpResponse(f"code = {request.GET.get('code')}")
-
-
-# 📌 Award/Certification 전용 Router
-router = DefaultRouter()
-router.register(r"awards", AwardViewSet, basename="award")
-router.register(r"certifications", CertificationViewSet, basename="certification")
-router.register(r"globalexps", GlobalExpViewSet, basename="globalexp")
-router.register(r"foreignlangs", ForeignLangViewSet, basename="foreignlang")
-
-
-=======
->>>>>>> 1027
 urlpatterns = [
     path("", health, name="health-root"),
     path("health/", health, name="health"),
     path("admin/", admin.site.urls),
 
-<<<<<<< HEAD
-    # ✅ allauth 라우트 등록
-    path("accounts/", include("allauth.urls")),
-
-    # GOOGLE
-    path("api/auth/google/login/", GoogleAuthStart.as_view(), name="google_auth_start"),
-    path("api/auth/google/callback/", GoogleAuthCallback.as_view(), name="google_auth_callback"),
-
-    # GITHUB
-    path("api/auth/github/login/", GitHubAuthStart.as_view(), name="github_auth_start"),
-    path("api/auth/github/callback/", GitHubAuthCallback.as_view(), name="github_auth_callback"),
-
-    # 로그인/회원가입 + dj-rest-auth
-=======
     path("accounts/", include("allauth.urls")),
 
     #GOOGLE
     path("api/v1/auth/google/", GoogleLogin.as_view(), name="google_login"),
     path("api/v1/auth/google/callback/", GoogleLoginCallback.as_view(), name="google_login_callback"),
 
->>>>>>> 1027
     path("api/auth/login/", CustomLoginView.as_view(), name="rest_login"),
     path("api/auth/registration/", CustomRegisterView.as_view(), name="rest_register"),
     path("api/password_reset/", include("django_rest_passwordreset.urls", namespace="password_reset")),
